c9090428d2e97cc7d6c3b92d0909716e288572d5
cygaar <97691933+cygaar@users.noreply.github.com>
2024-03-12 18:12:29 -0400

    Update (#2)

--- src/lib.rs
+++ src/lib.rs
@@ -2,15 +2,16 @@
 extern crate alloc;
 extern crate mini_alloc;
 
-mod erc721a;
-
 use crate::erc721a::{ERC721Params, ERC721};
 use alloc::{format, string::String, vec::Vec};
 use mini_alloc::MiniAlloc;
 use stylus_sdk::{alloy_primitives::U256, msg, prelude::*};
 
+#[cfg(target_arch = "wasm32")]
 #[global_allocator]
 static ALLOC: MiniAlloc = MiniAlloc::INIT;
+
+mod erc721a;
 
 pub struct SampleParams;
 
@@ -33,62 +34,36 @@ sol_storage! {
     pub struct SampleNFT {
         #[borrow] // Allows erc721 to access SampleNFT's storage and make calls
         ERC721<SampleParams> erc721a;
-        uint256 total_supply;
     }
 }
 
 // Rust implementation of this SampleNFT Solidity contract:
 
 // pragma solidity ^0.8.21;
-// import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+// import "erc721a/contracts/ERC721A.sol";
 
-// contract SampleNFT is ERC721 {
-//     uint256 public totalSupply;
+// contract SampleNFT is ERC721A {
+//     constructor() ERC721A("Sample NFT", "SAMPLE") {}
 
-//     constructor() ERC721("Sample NFT", "SAMPLE") {}
-
-//     function mintLoop(uint256 qty) external {
-//         uint256 supply = totalSupply;
-//         for (uint256 i; i < qty; ) {
-//             unchecked {
-//                 _mint(msg.sender, supply++);
-//                 ++i;
-//             }
-//         }
-//         totalSupply = supply;
+//     function mint(uint256 qty) external {
+//         _mint(msg.sender, qty);
 //     }
 
 //     function burn(uint256 tokenId) external {
-//         _burn(tokenId);
-//         unchecked {
-//             --totalSupply;
-//         }
+//         _burn(tokenId, true);
 //     }
 // }
 
 #[external]
 #[inherit(ERC721<SampleParams>)]
 impl SampleNFT {
-    pub fn total_supply(&self) -> Result<U256, Vec<u8>> {
-        Ok(self.total_supply.get())
-    }
-
-    pub fn mint_loop(&mut self, qty: U256) -> Result<(), Vec<u8>> {
-        let supply = self.total_supply.get();
-        let supply: u32 = supply.try_into().unwrap();
-        let qty: u32 = qty.try_into().unwrap();
-
-        for i in 0..qty.try_into().unwrap() {
-            self.erc721a._mint(msg::sender(), U256::from(supply + i))?;
-        }
-        self.total_supply.set(U256::from(supply + qty));
+    pub fn mint(&mut self, qty: U256) -> Result<(), Vec<u8>> {
+        self.erc721a._mint(msg::sender(), qty)?;
         Ok(())
     }
 
     pub fn burn(&mut self, token_id: U256) -> Result<(), Vec<u8>> {
         self.erc721a._burn(token_id, true)?;
-        let supply = self.total_supply.get();
-        self.total_supply.set(supply - U256::from(1));
         Ok(())
     }
 }
